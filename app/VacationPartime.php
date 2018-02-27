b44c51f3fe409fb4368124c0c448b4d34322ed76
thiennh <thiennh@haposoft.com>
2018-02-27 14:44:43 +0700

    fix done

--- app/VacationPartime.php
+++ app/VacationPartime.php
@@ -9,7 +9,7 @@ class VacationPartTime extends Model
 {
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
-    protected $table = 'vacation_parttimes';
+    protected $table = 'vacation_partimes';
 
     public function user(){
     	return $this->belongsTo(User::class);
